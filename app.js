d880a91e28a5d45e3646e02094e650641fc05576
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-11 15:45:57 +0300

    finish lecture 20 section 6

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(morgan('dev'));
 // In creating new tour/user this will help
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
