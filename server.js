edeff25a6b6448e2d822cf8cd1acce143b7804a1
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-12 12:53:40 +0300

    finish section 8 lecture 2

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// This will read the variables and save them in nodejs environment variables
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// This will read the variables and save them in nodejs environment variables
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
