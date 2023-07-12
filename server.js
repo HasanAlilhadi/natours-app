d6243a6f6c36777773ebbcc049c5edd098acb7da
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-12 15:58:32 +0300

    finish section 8 lecture 5

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    unique: true,
   },
   rating: {
     type: Number,
@@ -31,6 +32,20 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`ERROR: ${err}`);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
