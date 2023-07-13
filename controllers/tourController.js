d6f26032e238111c0f39b2caf9cb7f3cfdb583c6
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-13 14:32:45 +0300

    finish section 8 lecture 15

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,26 @@ const Tour = require('./../models/tourModel.js');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    console.log(req.query, queryObj);
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // { difficulty: 'easy', duration: { $gte: 5 } }
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
