ade7a74d0cf8c72f4cda23ba08f77d0a61bf5532
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-14 19:14:56 +0300

    finish section 8 lecture 21

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.fiveCheap, tourController.getAllTours);
+router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
